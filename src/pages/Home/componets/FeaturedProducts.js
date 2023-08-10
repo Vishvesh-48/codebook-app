66d0a19b060fdb82aaf747e81519ef63b7320b32
Vishu <koltevishveshwar48@gmail.com>
2023-08-10 07:32:50 +0530

    error handle

--- src/pages/Home/componets/FeaturedProducts.js
+++ src/pages/Home/componets/FeaturedProducts.js
@@ -1,33 +1,41 @@
 import { useEffect, useState } from "react"
 import { ProductCard } from "../../../componet"
+import { toast } from "react-toastify"
 
 export const FeaturedProducts = () => {
- 
-   const [products,setProducts] = useState([])
 
-   useEffect(()=>{
-    async function fetchProducts (){
+  const [products, setProducts] = useState([])
 
-      const response  = await fetch(`${process.env.REACT_APP_HOST}/444/featured_products`)
-      const data = await response.json()
-      setProducts(data)
+  useEffect(() => {
+    async function fetchProducts() {
+
+      const response = await fetch(`${process.env.REACT_APP_HOST}/444/featured_products`)
+      if (!response.ok) {
+        throw { message: response.statusText, status: response.status } // eslint-disable-line
+      }
+      try {
+        const data = await response.json()
+        setProducts(data)
+      } catch (error) {
+        toast.error(error.message, {closeButton:true,position:"bottom-center"})
+      }
+
     }
     fetchProducts()
-   },[])
+  }, [])
 
-    return (
-      <section className="my-20">
-          <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Featured eBooks</h1>    
-          <div className="flex flex-wrap justify-center lg:flex-row">
-              {/* Product Card */}
-              
-              {products.map((product)=>(
+  return (
+    <section className="my-20">
+      <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Featured eBooks</h1>
+      <div className="flex flex-wrap justify-center lg:flex-row">
+        {/* Product Card */}
 
-                <ProductCard key={product.id} product={product} />
-              ))}
-              
-          </div>
-      </section>
-    )
-  }
-  
+        {products.map((product) => (
+
+          <ProductCard key={product.id} product={product} />
+        ))}
+
+      </div>
+    </section>
+  )
+}
