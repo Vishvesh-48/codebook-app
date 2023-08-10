a9b63c2159f3387c8b9f11f93936c6839ab5d2fd
Vishu <koltevishveshwar48@gmail.com>
2023-08-10 15:03:10 +0530

    loginAsGeust

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -37,9 +37,42 @@ export const Login = () => {
         sessionStorage.setItem("cbid", JSON.stringify(data.user.id))
       }
     } catch (error) {
-       toast.error(error.message , {closeButton : true , position: "bottom-center"})
+      toast.error(error.message, { closeButton: true, position: "bottom-center" })
     }
 
+  }
+
+  async function handleLoginGeust() {
+    email.current.value = process.env.REACT_APP_LOGINID
+    password.current.value = process.env.REACT_APP_PASSWORD
+
+
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value
+      }
+     console.log(authDetail)
+      const requestOption = {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(authDetail)
+      }
+
+      const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOption)
+      if (!response.ok) {
+        throw { message: response.statusText, status: response.status } // eslint-disable-line
+      }
+      const data = await response.json()
+      if (data.accessToken) {
+        sessionStorage.setItem("Token", JSON.stringify(data.accessToken))
+        sessionStorage.setItem("cbid", JSON.stringify(data.user.id))
+      }
+      data.accessToken ? navigate("/products") : toast.error(data)
+
+    } catch (error) {
+      toast.error(error.message, { closeButton: true, position: "bottom-center" })
+    }
 
   }
 
@@ -61,7 +94,7 @@ export const Login = () => {
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button onClick={handleLoginGeust} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   )
 }
